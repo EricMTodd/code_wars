ea66b0e55ea051fb037c6076034d4ecbc461b846
-thugnasty- <eterictodd@gmail.com>
2019-05-12 07:41:34 -0600

    completed kata sumOfNumbers

---
+++ sum_of_numbers/sumOfNumbers.js
@@ -0,0 +1,20 @@
+function GetSum(a, b) {
+  let temp = 0;
+  let sum = 0;
+
+  if (a > b) {
+    for (let i = b; i <= a; i++) {
+      temp = i;
+      sum += temp;
+    }
+  } else {
+    for (let i = a; i <= b; i++) {
+      temp = i;
+      sum += temp;
+    }
+  }
+  console.log(`Solution: ${sum}`);
+  return sum;
+}
+
+GetSum(-17, -17);

39af3d4d5d432b3fdab5154cd53b54eeab6f9c4b
-thugnasty- <eterictodd@gmail.com>
2019-05-12 06:55:31 -0600

    created new root repo

---
+++ the_last_shall_be_first/theFirstShallBeLast.js
@@ -0,0 +1,7 @@
+function removeChar(str) {
+  let result = str.slice(1, -1);
+  console.log(result);
+  return result;
+}
+
+removeChar("The first shall be last and the last shall be first.");
---
+++ vowel_count/vowelCount.js
@@ -0,0 +1,17 @@
+getCount = str => {
+  let vowelCount = 0;
+  let vowels = "aeiouAEIOU";
+
+  for (let i = 0; i < str.length; i++) {
+    for (let k = 0; k < vowels.length; k++) {
+      if (str[i] === vowels[k]) {
+        vowelCount++;
+      }
+    }
+  }
+
+  console.log(`The number of vowels in ${str} is ${vowelCount}`);
+  return vowelCount;
+};
+
+getCount("abracadabra");

9f87e1e5e1bcb608b6a813118867e294f3138f8a
-thugnasty- <eterictodd@gmail.com>
2019-05-14 09:54:45 -0600

    kata complete

--- iq_test/iqTest.js
+++ iq_test/iqTest.js
@@ -5,16 +5,12 @@ function iqTest(numbers) {
   let o = 0;
   let x = 0;
   let y = 0;
-  console.log("beginning new test");
-  console.log(arr);
 
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] % 2 === 0) {
-      console.log(`${arr[i]} is even`);
       e++;
       x = i + 1;
     } else if (arr[i] % 2 === 1) {
-      console.log(`${arr[i]} is odd`);
       o++;
       y = i + 1;
     }

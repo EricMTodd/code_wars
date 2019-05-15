fb16437d804e0f255465fc114144428f626b6c37
-thugnasty- <eterictodd@gmail.com>
2019-05-14 21:54:56 -0600

    kata underway

--- equal_sides_of_an_array/equalSidesOfAnArray.js
+++ equal_sides_of_an_array/equalSidesOfAnArray.js
@@ -1,12 +1,21 @@
 function findEvenIndex(arr) {
   let activeIndex = 0;
-  let leftSum = 0;
+  let arrLength = arr.length;
   let rightSum = 0;
+  let leftSum = 0;
+
   console.log(arr);
+  console.log(`arrLength: ${arrLength}`);
+
   // Test every index of the array
   for (let i = 0; i < arr.length; i++) {
     activeIndex = i;
     console.log(`activeIndex: ${activeIndex}`);
+    for (let j = i + 1; j < arrLength; j++) {
+      console.log(`arr[j]: ${arr[j]}`);
+      rightSum += arr[j];
+      console.log(`rightSum ${rightSum}`);
+    }
   }
   // Find the sum of all the numbers on either side of the index being evaluated in an attempt to find equal sums
   // If there is no equal sum, return -1

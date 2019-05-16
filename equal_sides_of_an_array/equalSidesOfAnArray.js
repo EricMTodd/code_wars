cba7ae99116ec5c44cfff72214f682c98faaccce
-thugnasty- <eterictodd@gmail.com>
2019-05-15 22:13:35 -0600

    kata complete

--- equal_sides_of_an_array/equalSidesOfAnArray.js
+++ equal_sides_of_an_array/equalSidesOfAnArray.js
@@ -8,34 +8,25 @@ function findEvenIndex(arr) {
   for (let i = 0; i < arr.length; i++) {
     rightSum = 0;
     leftSum = 0;
-    console.log(`i: ${i}`);
+
     if (i >= 1) {
       for (let k = 0; k < i; k++) {
-        console.log(`${k}: arr[k]: ${arr[k]}`);
         leftSum += arr[k];
-        console.log(`leftSum: ${leftSum}`);
       }
     }
     for (let j = i + 1; j < arr.length; j++) {
-      console.log(`${j}: arr[j]: ${arr[j]}`);
       if (j != undefined) {
         rightSum += arr[j];
-        console.log(`rightSum: ${rightSum}`);
       }
     }
     if (rightSum === leftSum) {
-      console.log("MATCH FOUND");
-      console.log(`rightSum: ${rightSum}`);
-      console.log(`leftSum: ${leftSum}`);
       solution = i;
-      console.log(`Solution is index ${solution}`);
-    } else {
-      console.log("NO MATCH");
-      console.log(`rightSum: ${rightSum}`);
-      console.log(`leftSum: ${leftSum}`);
-      solution = -1;
-      console.log(`No solution, returning ${solution}`);
+      console.log(`Match found at index ${solution}`);
     }
+  }
+  if (solution === undefined) {
+    console.log("No matches found, returning -1");
+    solution = -1;
   }
   return solution;
 }

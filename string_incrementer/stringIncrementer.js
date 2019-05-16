54736cc668779607a296914540be3c4e2e15123a
-thugnasty- <eterictodd@gmail.com>
2019-05-16 06:41:28 -0600

    starting kata

---
+++ string_incrementer/stringIncrementer.js
@@ -0,0 +1,10 @@
+function incrementString(strng) {
+  console.log(strng);
+}
+
+incrementString("foobar000");
+incrementString("foo");
+incrementString("foobar001");
+incrementString("foobar99");
+incrementString("foobar099");
+incrementString("");

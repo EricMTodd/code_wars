0b4b010645cc322f18bc307a7ff9d089f503b152
-thugnasty- <eterictodd@gmail.com>
2019-05-16 06:31:08 -0600

    kata complete

--- find_the_smallest_integer_in_the_array/findTheSmallestIntegerInTheArray.js
+++ find_the_smallest_integer_in_the_array/findTheSmallestIntegerInTheArray.js
@@ -1,5 +1,16 @@
 function findSmallestInt(args) {
+  let smallestInt = args[0];
   console.log(`args: ${args}`);
+
+  for (let i = 0; i < args.length; i++) {
+    for (let j = 0; j < args.length; j++) {
+      if (args[j] < smallestInt) {
+        smallestInt = args[j];
+        console.log(`smallestInt: ${smallestInt}`);
+      }
+    }
+  }
+  return smallestInt;
 }
 
 findSmallestInt([34, 15, 88, 2]);

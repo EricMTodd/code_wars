553a1269db5172dc2b217e94a331fcbf95dcd846
-thugnasty- <eterictodd@gmail.com>
2019-05-16 06:21:07 -0600

    starting kata

---
+++ find_the_smallest_integer_in_the_array/README.md
@@ -0,0 +1,3 @@
+# find_the_smallest_integer_in_the_array
+
+https://www.codewars.com/kata/55a2d7ebe362935a210000b2/train/javascript
---
+++ find_the_smallest_integer_in_the_array/findTheSmallestIntegerInTheArray.js
@@ -0,0 +1,11 @@
+function findSmallestInt(args) {
+  console.log(`args: ${args}`);
+}
+
+findSmallestInt([34, 15, 88, 2]);
+findSmallestInt([34, -345, -1, 100]);
+findSmallestInt([78, 56, 232, 12, 8]);
+findSmallestInt([78, 56, 232, 12, 18]);
+findSmallestInt([78, 56, 232, 412, 228]);
+findSmallestInt([78, 56, 232, 12, 0]);
+findSmallestInt([1, 56, 232, 12, 8]);

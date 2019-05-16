eb320f3bbee6152e6508223181eaff54bbb871a8
-thugnasty- <eterictodd@gmail.com>
2019-05-15 22:06:30 -0600

    kata in progress

--- equal_sides_of_an_array/equalSidesOfAnArray.js
+++ equal_sides_of_an_array/equalSidesOfAnArray.js
@@ -1,24 +1,43 @@
 function findEvenIndex(arr) {
+  let solution;
   let rightSum;
   let leftSum;
   console.log(`arr: ${arr}`);
   console.log(`arr.length: ${arr.length}`);
 
-  // console.log(`arr[2]: ${arr[2]}`);
-  // console.log(`arr[2 + 1]: ${arr[2 + 1]}`); // same as console.log(`arr[2 + 1]: ${arr[2 + 1]}`);
-
   for (let i = 0; i < arr.length; i++) {
+    rightSum = 0;
+    leftSum = 0;
     console.log(`i: ${i}`);
     if (i >= 1) {
       for (let k = 0; k < i; k++) {
-        console.log(`k: ${k}`);
+        console.log(`${k}: arr[k]: ${arr[k]}`);
+        leftSum += arr[k];
+        console.log(`leftSum: ${leftSum}`);
       }
     }
     for (let j = i + 1; j < arr.length; j++) {
-      console.log(`j: ${j}`);
+      console.log(`${j}: arr[j]: ${arr[j]}`);
+      if (j != undefined) {
+        rightSum += arr[j];
+        console.log(`rightSum: ${rightSum}`);
+      }
+    }
+    if (rightSum === leftSum) {
+      console.log("MATCH FOUND");
+      console.log(`rightSum: ${rightSum}`);
+      console.log(`leftSum: ${leftSum}`);
+      solution = i;
+      console.log(`Solution is index ${solution}`);
+    } else {
+      console.log("NO MATCH");
+      console.log(`rightSum: ${rightSum}`);
+      console.log(`leftSum: ${leftSum}`);
+      solution = -1;
+      console.log(`No solution, returning ${solution}`);
     }
   }
-  return;
+  return solution;
 }
 
 findEvenIndex([1, 2, 3, 4, 3, 2, 1]);

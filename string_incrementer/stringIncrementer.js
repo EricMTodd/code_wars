d815b5204e894f2b858769678ebea231770d1d4b
-thugnasty- <eterictodd@gmail.com>
2019-05-18 10:28:45 -0600

    kata complete

--- string_incrementer/stringIncrementer.js
+++ string_incrementer/stringIncrementer.js
@@ -2,43 +2,50 @@ function incrementString(strng) {
   console.log(`evaluating string: ${strng}`);
   let testCaseOne = "0123456789";
   let testCaseTwo = "abcdefghijklmnopqrstuvwxyz";
+  let zeros = "";
   let alphanum = "";
   let ints = "";
-  let newString = "";
+  let intsLength = 0;
   let newInt = "";
+  let newString = "";
 
   // Split the string between letters and integers by checking the value of each index and then adding letters and numbers into their own variable using concatination
   for (let i = 0; i < strng.length; i++) {
+    if (strng[i] === "0") {
+      zeros += strng[i];
+    }
     for (let j = 0; j < testCaseOne.length; j++) {
       if (strng[i] == testCaseOne[j]) {
         ints = ints + strng[i];
-        console.log(`ints: ${ints}`);
       }
+      intsLength = ints.length;
     }
     for (let k = 0; k < testCaseTwo.length; k++) {
       if (strng[i] == testCaseTwo[k]) {
         alphanum = alphanum + strng[i];
-        console.log(`alphanum: ${alphanum}`);
       }
     }
   }
+  console.log(`alphanum: ${alphanum}`);
+  console.log(`ints: ${ints}`);
+
   // Increment the number by 1
   if (ints[ints.length - 1] === undefined) {
     newInt = 1;
-    console.log(`newInt: ${newInt}`);
   } else {
-    newInt = Number(ints[ints.length - 1]);
+    newInt = Number(ints);
     newInt += 1;
-    console.log(`newInt: ${newInt}`);
-    console.log(typeof newInt);
+    newInt = newInt.toString();
+    newInt = zeros + newInt;
   }
-  // Rejoin string and integers while retaining the proper amount of 0s
-  // If the number is a 9, be sure to increment to the next tenth appropriately
-  console.log(ints);
-  ints = ints.slice(0, -1);
-  console.log(ints);
-  console.log("got here");
-  return;
+  if (newInt.length != intsLength && newInt[0] == "0") {
+    newInt = newInt.slice(1);
+  }
+
+  newString = alphanum + newInt;
+
+  console.log(`newSting: ${newString}`);
+  return newString;
 }
 
 incrementString("foobar000");
@@ -47,8 +54,3 @@ incrementString("foobar001");
 incrementString("foobar99");
 incrementString("foobar099");
 incrementString("");
-
-// let testString = "Testing"; // returns "Testing"
-// console.log(testString);
-// newString = testString + "123"; // returns "Testing123"
-// console.log(newString);

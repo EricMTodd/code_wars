f3ff07d15851c06a089d1fb48498a73de27a7b39
EricMTodd <eterictodd@gmail.com>
2019-05-21 03:38:27 -0600

    repo repaired

---
+++ give_me_a_diamond/README.md
@@ -0,0 +1,3 @@
+# give_me_a_diamond
+
+https://www.codewars.com/kata/give-me-a-diamond/train/javascript
---
+++ give_me_a_diamond/giveMeADiamond.js
@@ -0,0 +1,10 @@
+function diamond(n) {
+  console.log("we are legion");
+  console.log(n);
+  // return diam;
+}
+
+diamond(3);
+diamond(2);
+diamond(-3);
+diamond(0);

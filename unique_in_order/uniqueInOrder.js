3864f51cd916a22c6bf79dd5bf42f67f686a9101
EricMTodd <eterictodd@gmail.com>
2019-05-25 19:44:46 -0600

    adjusted function

--- unique_in_order/uniqueInOrder.js
+++ unique_in_order/uniqueInOrder.js
@@ -1,6 +1,6 @@
-uniqueInOrder = iterable => {
+uniqueInOrder = str => {
   //your code here - remember iterable can be a string or an array
-  console.log(iterable);
+  console.log(str);
 };
 
 uniqueInOrder("AAAABBBCCDAABBB");

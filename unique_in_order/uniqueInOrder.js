88e725abda936bc330322e737322b5da5efc9eb8
EricMTodd <eterictodd@gmail.com>
2019-05-25 19:55:07 -0600

    kata in progress

--- unique_in_order/uniqueInOrder.js
+++ unique_in_order/uniqueInOrder.js
@@ -1,6 +1,15 @@
 uniqueInOrder = str => {
-  //your code here - remember iterable can be a string or an array
+  //your code here - remember iterable can be a string or an array.
+  // Return an array that matches the given string without any repeating characters.
+  let current = "";
+  let previous = "";
+  let arr = [];
+
   console.log(str);
+  for (let i = 0; i < str.length; i++) {
+    current = i;
+    console.log(current);
+  }
 };
 
 uniqueInOrder("AAAABBBCCDAABBB");

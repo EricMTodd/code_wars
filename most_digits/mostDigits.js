0585826c3e5099801802f065fe87b790f81bc3e5
-thugnasty- <eterictodd@gmail.com>
2019-05-27 17:56:57 -0600

    kata in progress

--- most_digits/mostDigits.js
+++ most_digits/mostDigits.js
@@ -1,6 +1,17 @@
 function findLongest(array) {
   // code here
+  let current = "";
+  let testCase = "";
   console.log(array);
+  for (let i = 0; i < array.length; i++) {
+    current = array[i];
+    testCase = "" + current;
+    console.log(`testCase: ${testCase}`);
+    for (let k = 0; k < testCase.length; k++) {
+      console.log(testCase[k]);
+    }
+  }
+  return;
 }
 
 findLongest([1, 10, 100]);

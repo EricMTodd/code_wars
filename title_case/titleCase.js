7bbfbf9d990f504f7bddd9cb2dff946a36ac639e
EricMTodd <eterictodd@gmail.com>
2019-06-01 10:10:58 -0600

    starting kata

---
+++ title_case/titleCase.js
@@ -0,0 +1,8 @@
+function titleCase(title, minorWords) {
+  console.log(title, minorWords);
+}
+
+titleCase("");
+titleCase("a clash of KINGS", "a an the of");
+titleCase("THE WIND IN THE WILLOWS", "The In");
+titleCase("the quick brown fox");

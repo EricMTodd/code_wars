d5a8cd15058715248ad3a045f0e6d1a6f74b667e
-thugnasty- <eterictodd@gmail.com>
2019-06-07 23:11:39 -0600

    kata in progress

--- title_case/titleCase.js
+++ title_case/titleCase.js
@@ -3,15 +3,21 @@ function titleCase(title, minorWords) {
   // Evaluate string for spaces, if a character follows a space, capitalize it unless it is an exception.
   console.log(`title: ${title}, minorWords: ${minorWords}`);
   let newString = title.toLowerCase();
+  let firstLetter = "";
   let testCase = "";
   let finalString = "";
 
   for (let i = 0; i < newString.length; i++) {
     console.log(newString[i]);
-    if (newString[i] === " ") {
-      console.log("MATCH");
-      newString = newString[i + 1].toUpperCase();
-      console.log(`modified newString: ${newString}`);
+    if (newString[i] !== " ") {
+      testCase += newString[i];
+    } else {
+      firstLetter = testCase[0].toUpperCase();
+      console.log(`firstLetter: ${firstLetter}`);
+      console.log(`testCase: ${testCase}`);
+      // Use an if check to see if the letter needs to be capitalized.
+      // Remove the first letter of the testCase and replace it with the capitalized first letter.
+      // Take the new capitalized word and add it to the final string.
     }
   }
   return;

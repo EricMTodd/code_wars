6acb3be128d0ab5ad7f6e0ed5fa335caab91ed1a
EricMTodd <eterictodd@gmail.com>
2019-06-07 22:50:38 -0600

    migrating data

--- title_case/titleCase.js
+++ title_case/titleCase.js
@@ -1,5 +1,20 @@
 function titleCase(title, minorWords) {
-  console.log(title, minorWords);
+  // Create a proper title using the title and given contraints.
+  // Evaluate string for spaces, if a character follows a space, capitalize it unless it is an exception.
+  console.log(`title: ${title}, minorWords: ${minorWords}`);
+  let newString = title.toLowerCase();
+  let testCase = "";
+  let finalString = "";
+
+  for (let i = 0; i < newString.length; i++) {
+    console.log(newString[i]);
+    if (newString[i] === " ") {
+      console.log("MATCH");
+      newString = newString[i + 1].toUpperCase();
+      console.log(`modified newString: ${newString}`);
+    }
+  }
+  return;
 }
 
 titleCase("");

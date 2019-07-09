1a14d06eeba470060f9812fa147d831efe41d40b
-thugnasty- <eterictodd@gmail.com>
2019-07-08 19:22:04 -0600

    attempting different methods

--- title_case/titleCase.js
+++ title_case/titleCase.js
@@ -2,11 +2,6 @@ function titleCase(title, minorWords) {
   console.log(`title: ${title}, minorWords: ${minorWords}`);
 
   let solution = "";
-  let newTitle = "";
-  let newMinorWords = "";
-  let titleArray = [];
-  let minorWordsArray = [];
-  let titleIndex = "";
 
   if (
     // Fix bug where last string is set to undefined because the minorWords are setting the afore mentioned value.
@@ -19,27 +14,15 @@ function titleCase(title, minorWords) {
     solution = undefined;
   } else {
     console.log(`IF ROUTE 2`);
-    newTitle = title.toLowerCase();
-    titleArray = newTitle.split(" ");
-    console.log(`titleArray: ${titleArray}`);
-    newMinorWords = minorWords.toLowerCase();
-    minorWordsArray = newMinorWords.split(" ");
-    console.log(`minorWordsArray: ${minorWordsArray}`);
-  }
-  // Always capitalize the first letter of the first word.
-  // Evaluate the active index against each of the test cases to see if the words must be title cased.
-  for (let i = 0; i < titleArray.length; i++) {
-    titleIndex = titleArray[i];
-    console.log(`titleIndex: ${titleIndex}`);
-    console.log(`titleIndex[0]: ${titleIndex[0]}`);
-    for (let k = 0; k < minorWordsArray.length; k++) {
-      console.log(`minorWordsArray[k]: ${minorWordsArray[k]}`);
-      if (titleArray[i] === minorWordsArray[k]) {
-        console.log("MATCH");
-      }
+    title = title.toLowerCase().split(" ");
+    console.log(`title: ${title}`);
+    minorWords = minorWords.toLowerCase().split(" ");
+    console.log(`minorWords: ${minorWords}`);
+    for (let i = 0; i < title.length; i++) {
+      title[i] = title[i].charAt(0).toUpperCase() + title[i].slice(1);
     }
+    solution = title;
   }
-
   console.log(`solution: ${solution}`);
   return solution;
 }

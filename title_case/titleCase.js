9a86852bfc293d6e55b1770635a7784f7e4965ba
EricMTodd <eterictodd@gmail.com>
2019-07-13 14:31:14 -0600

    kata complete

--- title_case/titleCase.js
+++ title_case/titleCase.js
@@ -1,38 +1,25 @@
-function titleCase(title, minorWords) {
-  console.log(`title: ${title}, minorWords: ${minorWords}`);
-
-  let solution = "";
-
-  if (
-    // Fix bug where last string is set to undefined because the minorWords are setting the afore mentioned value.
-    title === "" ||
-    title === undefined ||
-    minorWords === "" ||
-    minorWords === undefined
-  ) {
-    console.log(`IF ROUTE 1`);
-    solution = undefined;
-  } else {
-    console.log(`IF ROUTE 2`);
-    title = title.toLowerCase().split(" ");
-    console.log(`title: ${title}`);
-    minorWords = minorWords.toLowerCase().split(" ");
-    console.log(`minorWords: ${minorWords}`);
-    for (let i = 0; i < title.length; i++) {
-      // for (let k = 0; k < minorWords.length; k++) {
-      //   console.log(`minorWords[k]: ${minorWords[k]}`);
-      //   if (title[i] === minorWords[k]) {
-      //     console.log("MATCH");
-      //   }
-      // }
-      title[i] = title[i].charAt(0).toUpperCase() + title[i].slice(1);
-    }
-    title = title.join(" ");
-    solution = title;
+function titleCase(title, minorWords = "") {
+  if (title.length === 0) {
+    return "";
   }
-  console.log(`solution: ${solution}`);
-  return solution;
+
+  let minorArray = minorWords.toLowerCase().split(" ");
+  let titleArray = title.toLowerCase().split(" ");
+
+  titleArray[0] = capitalize(titleArray[0]);
+
+  for (let i = 1; i < titleArray.length; i++) {
+    if (minorArray.indexOf(titleArray[i]) === -1) {
+      titleArray[i] = capitalize(titleArray[i]);
+    }
+  }
+
+  return titleArray.join(" ");
 }
+
+let capitalize = str => {
+  return str[0].toUpperCase() + str.slice(1);
+};
 
 titleCase("");
 titleCase("a clash of KINGS", "a an the of");

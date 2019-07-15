d175ef9834084b3be8baad6ee8c6e88fbbe18177
EricMTodd <eterictodd@gmail.com>
2019-07-15 07:19:55 -0600

    kata in progress

--- get_the_middle_character/getTheMiddleCharacter.js
+++ get_the_middle_character/getTheMiddleCharacter.js
@@ -1,8 +1,19 @@
 getMiddle = str => {
   console.log(str);
+
+  let position;
+  let length;
+
+  if (str.length % 2 == 1) {
+    position = str.length / 2;
+    length = 1;
+  } else {
+    position = str.length / 2 - 1;
+    length = 2;
+  }
+  return str.substring(position, position + length);
 };
 
-getMiddle("Hello World!");
 getMiddle("test");
 getMiddle("testing");
 getMiddle("middle");

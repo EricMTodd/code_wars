c999ca8e7fb175c5a47ca459645d3ba5a47deffe
-thugnasty- <eterictodd@gmail.com>
2019-07-15 23:31:28 -0600

    kata complete

--- alphabet_war/alphabetWar.js
+++ alphabet_war/alphabetWar.js
@@ -1,8 +1,41 @@
 function alphabetWar(fight) {
-  console.log(fight);
+  let leftValues = {
+    w: 4,
+    p: 3,
+    b: 2,
+    s: 1
+  };
+
+  let rightValues = {
+    m: 4,
+    q: 3,
+    d: 2,
+    z: 1
+  };
+
+  let leftSum = 0;
+  let rightSum = 0;
+
+  for (let i = 0; i < fight.length; i++) {
+    let target = fight[i];
+
+    if (rightValues[target]) {
+      rightSum += rightValues[target];
+    } else if (leftValues[target]) {
+      leftSum += leftValues[target];
+    }
+  }
+
+  if (rightSum > leftSum) {
+    return "Right side wins!";
+  } else if (leftSum > rightSum) {
+    return "Left side wins!";
+  } else {
+    return "Let's fight again!";
+  }
 }
 
-alphabetWar("z");
-alphabetWar("zdqmwpbs");
-alphabetWar("zzzzs");
-alphabetWar("wwwwww");
+console.log(alphabetWar("z"));
+console.log(alphabetWar("zdqmwpbs"));
+console.log(alphabetWar("zzzzs"));
+console.log(alphabetWar("wwwwww"));
--- get_the_middle_character/getTheMiddleCharacter.js
+++ get_the_middle_character/getTheMiddleCharacter.js
@@ -4,9 +4,11 @@ getMiddle = str => {
 
   if (str.length % 2 == 1) {
     position = str.length / 2;
+    console.log(`ROUTE 1: ${position}`);
     length = 1;
   } else {
     position = str.length / 2 - 1;
+    console.log(`ROUTE 2: ${position}`);
     length = 2;
   }
   return str.substring(position, position + length);

ccf5dab8b5c2ae9103f78974fd84d1e8d945c836
EricMTodd <eterictodd@gmail.com>
2019-07-25 20:07:21 -0600

    kata complete

--- python/list_filtering/list_filtering.py
+++ python/list_filtering/list_filtering.py
@@ -1,7 +1,8 @@
 def filter_list(l):
-    print(l)
+    ans = [x for x in l if type(x) == int]
+    return ans
 
 
-filter_list([1, 2, 'a', 'b'])
-filter_list([1, 'a', 'b', 0, 15])
-filter_list([1, 2, 'aasf', '1', '123', 123])
+print(filter_list([1, 2, 'a', 'b']))
+print(filter_list([1, 'a', 'b', 0, 15]))
+print(filter_list([1, 2, 'aasf', '1', '123', 123]))

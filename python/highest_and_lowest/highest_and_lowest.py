02ee416e402a6b4f3e03703f7d4f00c5bf60df4a
-thugnasty- <eterictodd@gmail.com>
2019-08-01 17:13:12 -0600

    kata complete

--- python/highest_and_lowest/highest_and_lowest.py
+++ python/highest_and_lowest/highest_and_lowest.py
@@ -1,17 +1,14 @@
 def high_and_low(numbers):
-    min = 0
-    max = 0
     numbers = numbers.split(" ")
-    for i in numbers:
+    for i in range(0, len(numbers)):
+        numbers[i] = int(numbers[i])
+    numbers.sort()
+    solution = str(numbers[-1]) + " " + str(numbers[0])
+    print(solution)
 
-        i = int(i)
-        if i > max:
-            max = i
-        if i < min:
-            min = i
-
-    numbers = str(max) + " " + str(min)
-    return numbers
+    return solution
 
 
 high_and_low("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6")
+high_and_low(
+    "712 463 3109 2235 2792 1497 2601 2608 2404 2698 2836 637 2102 2977 916 498 1639 1283 2429 2079 78 1958")

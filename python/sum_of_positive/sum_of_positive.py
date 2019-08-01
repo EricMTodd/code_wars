c0019a6e22921769689c1e4d9d76f00bb0616c60
-thugnasty- <eterictodd@gmail.com>
2019-08-01 17:18:19 -0600

    kata in progress

---
+++ python/sum_of_positive/sum_of_positive.py
@@ -0,0 +1,10 @@
+def positive_sum(arr):
+    # Your code here
+    return 0
+
+
+positive_sum([1, 2, 3, 4, 5])
+positive_sum([1, -2, 3, 4, 5])
+positive_sum([-1, 2, 3, 4, -5])
+positive_sum([])
+positive_sum([-1, -2, -3, -4, -5])

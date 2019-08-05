654c40d88f44d6dbe97d546ccb2cc74a4ac44f88
-thugnasty- <eterictodd@gmail.com>
2019-08-05 11:40:00 -0600

    starting kata

---
+++ python/which_are_in/which_are_in.py
@@ -0,0 +1,9 @@
+def in_array(array1, array2):
+    # your code
+    return []
+
+
+a1 = ["live", "arp", "strong"]
+a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+r = ['arp', 'live', 'strong']
+in_array(a1, a2)

843ef6097297fcdcb802881a95564c007b98c506
-thugnasty- <eterictodd@gmail.com>
2019-08-05 13:20:52 -0600

    kata in progress

--- python/which_are_in/which_are_in.py
+++ python/which_are_in/which_are_in.py
@@ -1,26 +1,5 @@
 def in_array(array1, array2):
-    letters = "abcdefghijklmnopqrstuvwxyz"
-    target = 0
-    print(target)
-    prev = -1
-    print(prev)
-
-    for i in letters:
-        target = letters.index(i)
-        # print(i)
-        # print(letters.index(i))
-        # print(array1[0][0])
-        for j in array1:
-            # print(j[0])
-            if j[0] == i:
-                # print(i)
-                # print(target)
-                if target < prev:
-                    print(target)
-                else:
-                    prev = target
-                    print(prev)
-
+    # new code time
     return
 
 

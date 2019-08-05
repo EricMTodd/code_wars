95218b80cb7160745b9db99d4f8859fe46806a69
-thugnasty- <eterictodd@gmail.com>
2019-08-05 10:53:54 -0600

    kata complete

--- python/find_the_odd_int/find_the_odd_int.py
+++ python/find_the_odd_int/find_the_odd_int.py
@@ -1,10 +1,8 @@
 def find_it(seq):
-
     for i in seq:
-        count = seq.count(i)
-
-    print(solution)
-    return solution
+        if seq.count(i) % 2 != 0:
+            print(i)
+            return i
 
 
 find_it([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5])

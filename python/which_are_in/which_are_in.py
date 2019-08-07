05f0093dc743bb3a781fe115f2631446e13dcbcf
EricMTodd <eterictodd@gmail.com>
2019-08-06 21:49:11 -0600

    kata in progress

--- python/which_are_in/which_are_in.py
+++ python/which_are_in/which_are_in.py
@@ -1,14 +1,26 @@
 def in_array(array1, array2):
     solution = []
 
+    # try .sorted() to sort the strings
+
     for i in array1:
-        substr = array1[i]
+        substr = i
+        print(substr)
         for j in array2:
-            if array2[j].index(substr) != -1:
-                solution.append(array1[i])
-                break
-    print(solution.sort())
-    return solution.sort()
+            print(j)
+
+    # for i in array1:
+    #     substr = i
+    #     print(substr)
+    # for j in array2:
+    # print(j.index(substr))
+    # if j.index(substr) != -1:
+    #     solution.append(i)
+    # break
+    # print(solution.sort())
+
+    # return solution
+    return
 
 
 a1 = ["live", "arp", "strong"]

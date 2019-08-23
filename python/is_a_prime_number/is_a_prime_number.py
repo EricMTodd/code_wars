360bcf10b55f20c183fd6db7e6bdcd36e7abd89b
EricMTodd <eterictodd@gmail.com>
2019-08-22 20:35:12 -0600

    kata in progress

--- python/is_a_prime_number/is_a_prime_number.py
+++ python/is_a_prime_number/is_a_prime_number.py
@@ -1,34 +1,23 @@
 def is_prime(num):
-    pass  # TODO
+    print(num)
 
 
-@Test.describe("Basic")
-def basic():
-
-    @Test.it("Basic tests")
-    def basic_tests():
-        Test.assert_equals(is_prime(0),  False, "0  is not prime")
-        Test.assert_equals(is_prime(1),  False, "1  is not prime")
-        Test.assert_equals(is_prime(2),  True, "2  is prime")
-        Test.assert_equals(is_prime(73), True, "73 is prime")
-        Test.assert_equals(is_prime(75), False, "75 is not prime")
-        Test.assert_equals(is_prime(-1), False, "-1 is not prime")
-
-    @Test.it("Test prime")
-    def test_prime():
-        Test.assert_equals(is_prime(3),  True, "3  is prime")
-        Test.assert_equals(is_prime(5),  True, "5  is prime")
-        Test.assert_equals(is_prime(7),  True, "7  is prime")
-        Test.assert_equals(is_prime(41), True, "41 is prime")
-        Test.assert_equals(is_prime(5099), True, "5099 is prime")
-
-    @Test.it("Test not prime")
-    def test_not_prime():
-        Test.assert_equals(is_prime(4),  False, "4  is not prime")
-        Test.assert_equals(is_prime(6),  False, "6  is not prime")
-        Test.assert_equals(is_prime(8),  False, "8  is not prime")
-        Test.assert_equals(is_prime(9), False, "9 is not prime")
-        Test.assert_equals(is_prime(45), False, "45 is not prime")
-        Test.assert_equals(is_prime(-5), False, "-5 is not prime")
-        Test.assert_equals(is_prime(-8), False, "-8 is not prime")
-        Test.assert_equals(is_prime(-41), False, "-41 is not prime")
+is_prime(0)
+is_prime(1)
+is_prime(2)
+is_prime(73)
+is_prime(75)
+is_prime(-1)
+is_prime(3)
+is_prime(5)
+is_prime(7)
+is_prime(41)
+is_prime(5099)
+is_prime(4)
+is_prime(6)
+is_prime(8)
+is_prime(9)
+is_prime(45)
+is_prime(-5)
+is_prime(-8)
+is_prime(-41)

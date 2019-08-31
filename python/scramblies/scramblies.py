2067073d132afde561a4884f8cf6b5c5a2587d4a
EricMTodd <eterictodd@gmail.com>
2019-08-31 05:16:35 -0600

    beginning kata

---
+++ python/scramblies/scramblies.py
@@ -0,0 +1,9 @@
+def scramble(s1, s2):
+    # your code here
+
+
+scramble('rkqodlw', 'world')
+scramble('cedewaraaossoqqyt', 'codewars')
+scramble('katas', 'steak')
+scramble('scriptjava', 'javascript')
+scramble('scriptingjava', 'javascript')

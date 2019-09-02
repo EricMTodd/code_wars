0d6c642c741131d19af290b93bfb364dcfb75aa6
-thugnasty- <eterictodd@gmail.com>
2019-09-02 09:04:39 -0600

    starting kata

---
+++ python/integers_recreation_one/integers_recreation_one.py
@@ -0,0 +1,7 @@
+def list_squared(m, n):
+    print(m, n)
+
+
+list_squared(1, 250)  # [[1, 1], [42, 2500], [246, 84100]]
+list_squared(42, 250)  # [[42, 2500], [246, 84100]]
+list_squared(250, 500)  # [[287, 84100]]

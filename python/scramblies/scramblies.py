e0b8e6c8f03c69da54e60991327375cdf0c0010c
-thugnasty- <eterictodd@gmail.com>
2019-09-02 08:44:42 -0600

    kata complete

--- python/scramblies/scramblies.py
+++ python/scramblies/scramblies.py
@@ -1,8 +1,8 @@
 def scramble(s1, s2):
     for i in set(s2):
         if s1.count(i) < s2.count(i):
-            return print(False)
-    return print(True)
+            return False
+    return True
 
 
 scramble('rkqodlw', 'world')

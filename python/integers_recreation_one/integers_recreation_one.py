a1f1cdbbf6b6ec28298df7973c6fe0ea21c3fef3
EricMTodd <eterictodd@gmail.com>
2019-09-08 21:33:31 -0600

    kata in progress

--- python/integers_recreation_one/integers_recreation_one.py
+++ python/integers_recreation_one/integers_recreation_one.py
@@ -2,18 +2,28 @@
 
 
 def list_squared(m, n):
+    print('START')
     solution = []
     n = n + 1
 
     for i in range(m, n):
-        print(f'divisors of {i}:')
+        sum = 0
+        # print(f'divisors of {i}:')
         for j in range(1, i + 1):
             if i % j == 0:
-                print(j)
-
-        # print(2500 ** 0.5)
+                # print(j)
+                j = j * j
+                sum += j
+                # print(f'sum: {sum}')
+        root = sum ** 0.5
+        # print(f'root: {root}')
+        root = str(root)
+        # print(type(root))
+        if len(root) <= 5:
+            print(f'{i}, {sum}')
+    print('END')
 
 
 list_squared(1, 250)  # [[1, 1], [42, 2500], [246, 84100]]
-# list_squared(42, 250)  # [[42, 2500], [246, 84100]]
-# list_squared(250, 500)  # [[287, 84100]]
+list_squared(42, 250)  # [[42, 2500], [246, 84100]]
+list_squared(250, 500)  # [[287, 84100]]

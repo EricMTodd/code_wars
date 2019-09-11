c221b8fc1f452005f8775cad9b7be7e567c02852
-thugnasty- <eterictodd@gmail.com>
2019-09-11 12:01:31 -0600

    kata complete

--- python/integers_recreation_one/integers_recreation_one.py
+++ python/integers_recreation_one/integers_recreation_one.py
@@ -1,20 +1,39 @@
 # We want to find all integers between m and n whose sum of squared divisors is itself a square. 42 is such a number.
 
-# from math import ceil
+
+# Original Solution
+# def list_squared(m, n):
+#     solution = []
+#     for i in range(m, n):
+#         sum = 0
+#         for j in range(1, i + 1):
+#             if i % j == 0:
+#                 sum += j ** 2
+#         sqrt = sum ** 0.5
+#         if round(sqrt) == sqrt:
+#             list = [i, sum]
+#             solution.append(list)
+#     print(solution)
+#     return solution
+
+
+# Efficient Solution
+from math import sqrt
+from math import ceil
 
 
 def list_squared(m, n):
     solution = []
-
-    for i in range(m, n + 1):
+    for i in range(m, n):
         sum = 0
-        for j in range(1, i + 1):
+        for j in range(1, ceil(sqrt(i))):
             if i % j == 0:
-                j = j * j
-                sum += j
-        root = sum ** 0.5
-        root = str(root)
-        if len(root) <= 5:
+                sum += j ** 2
+                if i != 1:
+                    sum += (i // j) ** 2
+        if i == 1:
+            sum = 1
+        if round(sqrt(sum)) == sqrt(sum):
             list = [i, sum]
             solution.append(list)
     print(solution)

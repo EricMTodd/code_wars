bee534764abf9504e7da1cd98acd73d9bdaab880
EricMTodd <eterictodd@gmail.com>
2019-09-12 08:20:45 -0600

    kata in progress

--- python/find_the_missing_letter/find_the_missing_letter.py
+++ python/find_the_missing_letter/find_the_missing_letter.py
@@ -1,5 +1,8 @@
 def find_missing_letter(chars):
-    return print(chars)
+    print("START")
+    letters = "abcdefghijklmnopqrstuvwxyz"
+    new_chars = "".join(chars)
+    new_chars = new_chars.lower()
 
 
 find_missing_letter(['a', 'b', 'c', 'd', 'f'])

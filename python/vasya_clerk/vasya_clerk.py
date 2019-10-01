b9a8528a8c2c1af225669a2dc47f594ab4c4553d
EricMTodd <eterictodd@gmail.com>
2019-10-01 15:51:46 -0600

    kata in progress

--- python/vasya_clerk/vasya_clerk.py
+++ python/vasya_clerk/vasya_clerk.py
@@ -1,5 +1,15 @@
 def tickets(people):
     print(people)
+    till = 0
+
+    for i in people:
+        till += i
+        print("till: " + str(till))
+        change = i - 25
+        print("change: " + str(change))
+        if change != 0:
+            till
+        print("YES")
 
 
 tickets([25, 25, 50])

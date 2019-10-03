c70326e1ab66025eab925e49e90256cc3d70bd7e
-thugnasty- <eterictodd@gmail.com>
2019-10-02 22:08:10 -0600

    kata in progress

--- python/vasya_clerk/vasya_clerk.py
+++ python/vasya_clerk/vasya_clerk.py
@@ -1,4 +1,5 @@
 def tickets(people):
+    print(people)
     till = {
         25: 0,
         50: 0,
@@ -6,23 +7,40 @@ def tickets(people):
     }
 
     for i in people:
-
+        print("i: " + str(i))
         if i == 25:
             till[25] += 1
+            print("till: " + str(till))
         elif i == 50:
+            till[50] += 1
             if till[25] < 1:
                 print("NO")
                 return "NO"
-            till[50] += 1
             till[25] -= 1
+            print("till: " + str(till))
         else:
-            if till[50] >= 1 and till[25] >= 1:
-                till[100] += 1
-                till[50] -= 1
-                till[25] -= 1
+            till[100] += 1
+            if till[50] < 1:
+                if till[25] >= 3:
+                    till[25] -= 3
+                    print("till: " + str(till))
+                elif till[50] >= 1 and till[25] >= 1:
+                    till[50] -= 1
+                    till[25] -= 1
+                    print("till: " + str(till))
+                else:
+                    print("NO")
+                    return "NO"
 
-    people_sum = sum(people)
+    print("YES")
+    return "YES"
 
 
 tickets([25, 25, 50])
 tickets([25, 100])
+tickets([25, 25, 25, 25, 25, 100, 100])
+tickets([25, 50, 25, 100, 25, 25, 50, 100, 25, 25, 25, 100,
+         25, 25, 50, 100, 25, 50, 25, 100, 25, 50, 50, 50])
+tickets([25, 25, 25, 100, 25, 25, 25, 100, 25,
+         25, 50, 100, 25, 25, 50, 100, 50, 50])
+tickets([25, 25, 25, 25, 25, 25, 25, 50, 50, 50, 100, 100, 100, 100])

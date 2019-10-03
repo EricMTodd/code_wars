04244c06b34a9bdb7835c1e38ebace466559e1f2
EricMTodd <eterictodd@gmail.com>
2019-10-02 21:24:29 -0600

    kata in progress

--- python/vasya_clerk/vasya_clerk.py
+++ python/vasya_clerk/vasya_clerk.py
@@ -1,15 +1,27 @@
 def tickets(people):
-    print(people)
-    till = 0
+    till = {
+        25: 0,
+        50: 0,
+        100: 0
+    }
 
     for i in people:
-        till += i
-        print("till: " + str(till))
-        change = i - 25
-        print("change: " + str(change))
-        if change != 0:
-            till
-        print("YES")
+
+        if i == 25:
+            till[25] += 1
+        elif i == 50:
+            if till[25] < 1:
+                print("NO")
+                return "NO"
+            till[50] += 1
+            till[25] -= 1
+        else:
+            if till[50] >= 1 and till[25] >= 1:
+                till[100] += 1
+                till[50] -= 1
+                till[25] -= 1
+
+    people_sum = sum(people)
 
 
 tickets([25, 25, 50])

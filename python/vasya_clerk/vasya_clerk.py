221fa764b4d25b4799f53853c7ae140d5e03d174
-thugnasty- <eterictodd@gmail.com>
2019-10-13 19:16:37 -0600

    kata complete

--- python/vasya_clerk/vasya_clerk.py
+++ python/vasya_clerk/vasya_clerk.py
@@ -1,5 +1,4 @@
 def tickets(people):
-    print(people)
     till = {
         25: 0,
         50: 0,
@@ -7,30 +6,24 @@ def tickets(people):
     }
 
     for i in people:
-        print("i: " + str(i))
         if i == 25:
             till[25] += 1
-            print("till: " + str(till))
         elif i == 50:
             till[50] += 1
             if till[25] < 1:
                 print("NO")
                 return "NO"
             till[25] -= 1
-            print("till: " + str(till))
         else:
             till[100] += 1
-            if till[50] < 1:
-                if till[25] >= 3:
-                    till[25] -= 3
-                    print("till: " + str(till))
-                elif till[50] >= 1 and till[25] >= 1:
-                    till[50] -= 1
-                    till[25] -= 1
-                    print("till: " + str(till))
-                else:
-                    print("NO")
-                    return "NO"
+            if till[50] >= 1 and till[25] >= 1:
+                till[50] -= 1
+                till[25] -= 1
+            elif till[25] >= 3:
+                till[25] -= 3
+            else:
+                print("NO")
+                return "NO"
 
     print("YES")
     return "YES"

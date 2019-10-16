a9001d2c1dbf918b20afd9a3406b2b1b9a026e2e
-thugnasty- <eterictodd@gmail.com>
2019-10-16 17:44:12 -0600

    kata complete

--- python/replace_with_alphabet_position/replace_with_alphabet_position.py
+++ python/replace_with_alphabet_position/replace_with_alphabet_position.py
@@ -1,6 +1,23 @@
 def alphabet_position(text):
     print(text)
 
+    text = text.lower()
+
+    letters = "abcdefghijklmnopqrstuvwxyz"
+
+    solution = ""
+
+    for i in text:
+        for j in letters:
+
+            if i == j:
+                index = str(letters.index(i) + 1)
+                solution += index + " "
+
+    solution = solution[:-1]
+    print(solution)
+    return solution
+
 
 alphabet_position("The sunset sets at twelve o' clock.")
 alphabet_position("The narwhal bacons at midnight.")

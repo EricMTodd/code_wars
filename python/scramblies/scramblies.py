b733150687998f9ea42eaa82fe498e4256a4df32
EricMTodd <eterictodd@gmail.com>
2019-10-17 18:17:23 -0600

    kata complete

--- python/scramblies/scramblies.py
+++ python/scramblies/scramblies.py
@@ -1,7 +1,11 @@
 def scramble(s1, s2):
+    print(s1, s2)
+
     for i in set(s2):
         if s1.count(i) < s2.count(i):
+            print(False)
             return False
+    print(True)
     return True
 
 
---
+++ python/string_transformer/string_transformer.py
@@ -0,0 +1,11 @@
+def string_transformer(s):
+    print(s)
+    return
+
+
+string_transformer("Example string")
+string_transformer("Example Input")
+string_transformer("To be OR not to be That is the Question")
+string_transformer("")
+string_transformer("You Know When  THAT  Hotline Bling")
+string_transformer(" A b C d E f G ")

e798cfeb6a4c93a19217d9c5fed130b6d26ca136
EricMTodd <eterictodd@gmail.com>
2019-10-17 18:41:20 -0600

    kata in progress

--- python/string_transformer/string_transformer.py
+++ python/string_transformer/string_transformer.py
@@ -1,6 +1,19 @@
 def string_transformer(s):
     print(s)
 
+    lower_case = "abcdefghijklmnopqrstuvwxyz"
+    upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    solution = ""
+
+    for i in s:
+        if i in lower_case:
+            i = i.upper()
+            print(i)
+        else:
+            i = i.lower()
+            print(i)
+
 
 string_transformer("Example string")
 string_transformer("Example Input")

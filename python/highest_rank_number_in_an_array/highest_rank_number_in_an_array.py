36e59f243ebdd9d19a47fdc43632f8c88a1a2e2b
-thugnasty- <eterictodd@gmail.com>
2019-11-03 19:46:39 -0700

    kata complete

--- python/highest_rank_number_in_an_array/highest_rank_number_in_an_array.py
+++ python/highest_rank_number_in_an_array/highest_rank_number_in_an_array.py
@@ -2,22 +2,29 @@ def highest_rank(arr):
     print(arr)
 
     max = 0
-    solution = 0
+    key_solution = 0
+    value_solution = 0
     dict = {}
 
     for i in arr:
         dict[i] = arr.count(i)
 
-    print(f'dict: {dict}')
-
     for key in dict:
-        print(f"{key}: {dict[key]}")
         if dict[key] > max:
             max = dict[key]
-            solution = key
+            key_solution = key
+            value_solution = dict[key]
 
-    print(solution)
+    for key in dict:
+        if dict[key] == value_solution:
+            if key > key_solution:
+                key_solution = key
+
+    print(f'key_solution: {key_solution}')
+    return key_solution
 
 
 highest_rank([12, 10, 8, 12, 7, 6, 4, 10, 12])
 highest_rank([12, 10, 8, 12, 7, 6, 4, 10, 10])
+highest_rank([12, 10, 8, 12, 7, 6, 4, 10, 12, 10])
+highest_rank([12, 10, 8, 12, 7, 6, 4, 10, 12, 13, 13, 13])

37d15b0bf3a7d043b515bef3124ffada98ec17ee
-thugnasty- <eterictodd@gmail.com>
2020-05-12 09:58:10 -0600

    working on kata

---
+++ ruby/mumbling/mumbling.rb
@@ -0,0 +1,16 @@
+def accum(s)
+  count_modifier = 0
+
+  puts
+  # puts s
+  s.each_char do |i|
+    puts i
+  end
+end
+
+
+accum("ZpglnRxqenU")
+accum("NyffsGeyylB")
+accum("MjtkuBovqrU")
+accum("EvidjUnokmM")
+accum("HbideVbxncC")
